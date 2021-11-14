7680bff79c694d36911e15cbddcd1873e30ce431
fbaliarda <33381234+fbaliarda@users.noreply.github.com>
2021-11-14 16:35:00 -0300

    Local blast

--- aminoacids_blast.py
+++ aminoacids_blast.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 from Bio.Blast import NCBIWWW
+from Bio.Blast.Applications import NcbiblastpCommandline
 import argparse
 
 def main():
@@ -10,12 +11,22 @@ def main():
         default='./output.blast', type=str)
     arg_parser.add_argument('-n', dest='size', help='BLAST hit list size', required=False,
         default=50, type=int)
+    arg_parser.add_argument('-db', dest='database', help='Local database name (if running locally)', required=False,
+        default=None, type=str)
+    arg_parser.add_argument('-blastp', dest='blastp', help='Path to blastp binary (if not already in PATH)', required=False,
+        default=None, type=str)
+    arg_parser.add_argument('-e', dest='evalue', help='Expect value for saving hits', required=False,
+        default=10, type=str)
+    
     args = vars(arg_parser.parse_args())
 
     input_path = args['input']
     output_path = args['output']
     hitlist_size = args['size']
-
+    database = args['database'];
+    blastp = args['blastp'] if args['blastp'] is not None else 'blastp'
+    evalue = args['evalue']
+    
     # Check if supported file extension
     file_extension = input_path.split(".")[-1]
     file_formats = {
@@ -27,26 +38,29 @@ def main():
         quit()
 
     file_format = file_formats[file_extension]
-    record = SeqIO.parse(input_path, file_format)
-    
-    try:
-        save_file = open(output_path, "w")
-
-        for rec in record:
-            print(f"Reading record:\n{rec}")
-            print("Querying NCBI database...")
-            result = NCBIWWW.qblast("blastp", "nr", rec.format("fasta"), hitlist_size=hitlist_size)
-            print("Saving BLAST report...")
-            save_file.write(result.read())
+    if database is None:
+        record = SeqIO.parse(input_path, file_format)
+        try:
+            save_file = open(output_path, "w")
+            for rec in record:
+                print(f"Reading record:\n{rec}")
+                print("Querying NCBI database...")
+                result = NCBIWWW.qblast("blastp", "nr", rec.format("fasta"), hitlist_size=hitlist_size, expect=evalue)
+                print("Saving BLAST report...")
+                save_file.write(result.read())
+                result.close()
+                print(f"BLAST report saved in {output_path}")
+            
+            save_file.close()
+        except Exception as e:
+            save_file.close()
             result.close()
-            print(f"BLAST report saved in {output_path}")
-        
-        save_file.close()
-        
-    except Exception as e:
-        save_file.close()
-        result.close()
-        print(e)
+            print(e)
+    else:
+        print(f"Querying local {database} database...")
+        blastx_cline = NcbiblastpCommandline(cmd=blastp, query=input_path, db=database, evalue=evalue,
+                                            outfmt=5, out=output_path, num_alignments=hitlist_size)
+        stdout, stderr = blastx_cline()
 
 if __name__ == "__main__":
     main()
