6fc1893bb08de14ba908f378bcf63ed931c9742e
Micaela <mbanfi@itba.edu.ar>
2021-11-15 22:36:21 -0300

    ej4

--- ej4/ej4.py
+++ ej4/ej4.py
@@ -2,6 +2,7 @@ import argparse
 import pickle
 import re
 from urllib.request import install_opener
+from xml.dom import minidom
 
 from bs4 import BeautifulSoup
 from bs4.builder import HTMLTreeBuilder
@@ -11,12 +12,13 @@ argparser = argparse.ArgumentParser(description='Blast Parser',
 
 argparser.add_argument('-p', '--pattern', type=str, required=True,help='Pattern determinado a buscar')
 argparser.add_argument('-i', '--input', help='path al archivo a parsear',
-                       type=str, default='edit.blast', required=False)
+                       type=str, default='output.blast', required=False)
 argparser.add_argument('-o', '--output_folder', help='carpeta donde guardar el archivo de salida',
                        type=str, default='./', required=False)
 args = vars(argparser.parse_args())
 
 try:
+    print("Parsing BLAST with input file",args['input'],", sequence",args['pattern'],"and output file",args['output_folder'] + 'ej4.blast')
     input=open(args['input'])
     output=open(args['output_folder'] + 'ej4.blast','w')
     pattern_input=args['pattern']
@@ -25,9 +27,9 @@ try:
     start='''<?xml version="1.0"?>
             <!DOCTYPE BlastOutput PUBLIC "-//NCBI//NCBI BlastOutput/EN" "http://www.ncbi.nlm.nih.gov/dtd/NCBI_BlastOutput.dtd">
             <BlastOutput>
-                <BlastOutput_iterations>
-                    <Iteration>
-                        <Iteration_hits>'''
+            <BlastOutput_iterations>
+            <Iteration>
+            <Iteration_hits>'''
 
     output.write(start)
     soup = BeautifulSoup(contents, 'html.parser')
@@ -36,24 +38,20 @@ try:
     pattern = lambda t: (pattern_input) in t
  
     all = soup.findAll('hit_def', text = pattern)
-    # parent=all.find_parent('hit')
     for val in all:
         parent=val.parent
-        # html=parent.prettify("utf-8")
-        print(parent)
+        html=parent.prettify("utf-8")
         output.write(str(parent))
 
     end ='''<Iteration_hits>
             <Iteration>
             <BlastOutput_iterations>
             <BlastOutput>'''
+    
     output.write(end)
-    # html = soup.prettify("utf-8")
-    # print(html)
-    # output.write(html)
-
     input.close()
     output.close()
+
 
 except Exception as e:
     output.close()
@@ -61,6 +59,8 @@ except Exception as e:
     print("Error")
     print(e)
  
+
+
 
  
 
